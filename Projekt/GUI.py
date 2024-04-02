ac4e9af3c7294401d2c92b5593b6dcdb2716f26a
Philip Pohlmann <m778336@hsu-hh.de>
2024-04-02 12:54:07 +0200

    Fenster (Main) lässt sich öffnen

---
+++ Projekt/GUI.py
@@ -0,0 +1,18 @@
+from PyQt5.QtWidgets import * 
+from PyQt5 import uic
+
+class SWRLRuleEditor(QMainWindow):
+
+    def __init__(self):
+        super(SWRLRuleEditor, self).__init__()
+        uic.loadUi("Projekt\MainWindow.ui", self)
+        self.show()
+
+def main():
+    project = QApplication([])
+    window = SWRLRuleEditor()
+    project.exec_()
+
+
+if __name__== "__main__":
+    main()
