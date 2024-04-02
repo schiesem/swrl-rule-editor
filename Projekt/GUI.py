6b7a087b2d9983f74bd7eea8a031f33a65e7e4d8
Philip Pohlmann <m778336@hsu-hh.de>
2024-04-02 18:02:23 +0200

    ontologieauswahl möglich

--- Projekt/GUI.py
+++ Projekt/GUI.py
@@ -1,7 +1,8 @@
 from PyQt5.QtWidgets import * 
 from PyQt5 import uic
 import os
-#import owlready2
+from owlready2 import *
+import owlready2
 from os import path
 
 
@@ -12,22 +13,48 @@ class SWRLRuleEditor(QMainWindow):
         uic.loadUi("Projekt\MainWindow.ui", self)
         self.show()
         ontology_names = list_files_in_folder("Ontologien")
+        self.comboBoxOntologies.addItem("none")
         self.comboBoxOntologies.addItems(ontology_names)
         self.comboBoxOntologies.currentIndexChanged.connect(self.ontologySelected)
-        
+        self.onto = get_ontology("file://" + "Ontologien\ghibli.rdf").load()                    #ontologie vor laden, wird dann später überschrieben, wenn eine ausgewählt wird. hier leere ontologie einfügen.
+
+        #ontoVorlage = get_ontology("file://" + "Ontologien\ghibli.rdf").load()
+        #print("...")
+        #print(return_elements(ontoVorlage.classes()))
 
     def ontologySelected(self):    
         selected_text = self.comboBoxOntologies.currentText()
-        print(selected_text)
-
+        print(selected_text)                                                        #testing
+        #print(ontoVorlage)
         
-        """file_name = f"{selected_text}"
+        file_name = f"{selected_text}"
         folder_path = r"Ontologien"
         file_path_save = os.path.join(folder_path, file_name)
         onto = get_ontology("file://" + file_path_save).load()
-        print("The ontology has been loaded")"""
+        print("The ontology has been loaded")                                       #testing
+        #print(return_elements(ontoVorlage.classes()))
+        #print("break")
+        print(return_elements(onto.classes()))
+        self.test = "Test neu!"
+        self.onto = onto
+        
 
+def return_elements(entities):
 
+    try:
+        #Ausgabemöglichkeiten return_elements(...):
+        #Eine Liste von  Klassen(onto.classes())
+        #                Instanzen(onto.individuals())
+        #                Properties(onto.properties())
+        #                ...
+
+        listeEntities = []
+        for entity in entities:
+            name = entity.name
+            listeEntities.append(name)
+        return listeEntities
+    except:
+        print("Error")
 
 
 def list_files_in_folder(folder_path):
@@ -47,6 +74,7 @@ def main():
     project = QApplication([])
     window = SWRLRuleEditor()
     project.exec_()
+    print("!")
 
 
 if __name__== "__main__":
